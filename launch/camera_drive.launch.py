2265b297d91b218e97ae970d1c4c9479d93bab92
SeungBeom046 <seungbeom@useungbeom-ui-MacBookPro.local>
2026-08-26 15:11:38 +0900

    카메라 gui 수정

--- launch/camera_drive.launch.py
+++ launch/camera_drive.launch.py
@@ -1,18 +1,22 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
+    realsense_launch = os.path.join(
+        get_package_share_directory('realsense2_camera'),
+        'launch',
+        'rs_launch.py',
+    )
+
     return LaunchDescription([
-        Node(
-            package='realsense2_camera',
-            executable='realsense2_camera_node',
-            name='camera',
-            parameters=[{
-                'enable_color': True,
-                'enable_depth': True,
-                'align_depth.enable': True,
-            }],
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(realsense_launch),
         ),
         Node(
             package='my_algo',
